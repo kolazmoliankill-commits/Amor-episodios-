5fefada537f82417ede0e9336b7b7b39b0722dd4
kolazmoliankill-commits <kolazmoliankill@gmail.com>
2026-04-25 00:06:06 -0500

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('v1').then(cache => cache.addAll(['./', './index.html', './escena.html', './historia.js', './manifest.json'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
+
